6eebfa477e125e3f0eb8bd42bec7094d8690b952
tojo chacko <tojo.c@directi.com>
2013-09-08 04:39:40 +0530

    Added a write Log function to make an error log entry

--- index.js
+++ index.js
@@ -10,47 +10,62 @@ app.post('/notify', function(request, response) {
   response.send('fail');
   var jsonObj = request.body;
   for(var i = 0;l=jsonObj.length,i<l;i++) {
-    var userId = jsonObj[i].user_ids;
+    var fbUserIdArr = jsonObj[i].user_ids;
     var message = jsonObj[i].message;
     var fbhref = jsonObj[i].href;
     var fbAccessToken = jsonObj[i].access_token;
   }
-  sendNotifications(userId, message, fbhref, fbAccessToken);
   response.send('Job Initiated');
+  sendNotifications(fbUserIdArr, message, fbhref, fbAccessToken);
 });
 
 app.listen(8080);
 console.log('Listening on port 8080');
 
-function sendNotifications(userId, message, fbhref, fbAccessToken)
+function sendNotifications(fbUserIdArr, message, fbhref, fbAccessToken)
 {
   var async = require('async'); 
-  async.mapLimit(userId, noofloops, notifyFB, function(error, result) {
+  async.mapLimit(fbUserIdArr, noofloops, notifyFB, function(error, result) {
     if(error) {
-      //write to a log file
-      console.log('FB request failed '+result);
+      writeErrorLog(JSON.stringify(result));
     }
     else {
       console.log(result);
     }
   });
 
-  function notifyFB(uid, callback) 
+  function notifyFB(fbuid, callback) 
   {
     var request = require('request');
-    var fburl = 'https://graph.facebook.com/'+uid+'/notifications?access_token='+fbAccessToken+'&template='+message+'&href='+fbhref;
+    var fburl = 'https://graph.facebook.com/'+fbuid+'/notifications?access_token='+fbAccessToken+'&template='+message+'&href='+fbhref;
     request({
       method: 'POST',
       uri: fburl}, function(error, response, body) {
         fbrespJson = JSON.parse(body);
         chkErrorCondition = fbrespJson.hasOwnProperty('error');
         if(chkErrorCondition) {
-          callback('FAIL', uid);
+          //error obj is of format
+          //{ error: { message: 'Unsupported post request.',
+          //type: 'GraphMethodException',
+          //code: 100 } }
+          fbrespJson.error.fbuid = uid;
+          callback('FAIL', fbrespJson.error);
         }
         else if(!chkErrorCondition) {
           callback(null, body);
         }
       });
   }
+}
+
+function writeErrorLog(logEntry)
+{
+  var fs = require('fs');
+  var log = fs.createWriteStream('log/error.log', {'flags': 'a'});
+
+  var date = new Date();
+  var dateTimeString = date.toDateString() + ' ' + date.toTimeString(); 
+  logEntry = dateTimeString + '  -  ' + logEntry;
+  log.write(logEntry);
 }
 
