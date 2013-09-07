e0cf6a73d0ea568ba3732fcf2143ba11dff6898b
tojo chacko <tojo.c@directi.com>
2013-09-08 01:53:58 +0530

    Added functionality to call fb api in parallel.

--- index.js
+++ index.js
@@ -4,31 +4,49 @@ var app = express();
 //without this we can't access request.body
 app.use(express.bodyParser());
 
+//api end point which apoorv will call
 app.post('/notify', function(request, response) {
-  //jsonObj = [{userid:{1,2,4}, message:'Hi there'}]
   var jsonObj = request.body;
   for(var i = 0;l=jsonObj.length,i<l;i++) {
     var userId = jsonObj[i].userid;
     var message = jsonObj[i].message;
     var fbendUrl = jsonObj[i].url;
   }
-  console.log(userId);
-  console.log(message);
-  sendNotifications();
+  //console.log(userId);
+  //console.log(message);
+  sendNotifications(userId, message, fbendUrl);
+  response.send('Job Initiated');
+});
+
+//test url for imitating fb calls
+app.get('/fbid', function(request, response) {
+  setTimeout(function() {}, 2000);
   response.send('success');
 });
 
 app.listen(8080);
 console.log('Listening on port 8080');
 
-function sendNotifications()
+function sendNotifications(userId, message, fbendUrl)
 {
-  //this i can use to call fb api
-  /*var request = require('request');
-  request('http://www.google.com', function(error, response, body) {
-  if(!error && response.statusCode == 200) {
-  console.log(body);
+  var async = require('async'); 
+  async.mapLimit(userId, 50, callfb, function(error, result) {
+    if(error == 'FAIL') {
+      console.log('FB request failed '+result);
+    }
+  });
+
+  function callfb(uid, callback) 
+  {
+    var request = require('request');
+    request('http://localhost:8080/fbid?id='+uid, function(error, response, body) {
+      if(!error && response.statusCode == 200) {
+        callback(null, response);
+      }
+      else if(error) {
+        callback('FAIL', uid);
+      }
+    });
   }
-  });*/
 }
 
